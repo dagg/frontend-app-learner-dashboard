2214d7a3bafd8d0be2af2f3758ef6e064228f45c
Dimitris <d.agelakis@gmail.com>
2025-01-27 16:19:43 +0200

    Removed the Prograns and Help buttons because they don't work!

--- src/containers/LearnerDashboardHeader/CollapsedHeader/CollapseMenuBody.jsx
+++ src/containers/LearnerDashboardHeader/CollapsedHeader/CollapseMenuBody.jsx
@@ -32,9 +32,9 @@ export const CollapseMenuBody = ({ isOpen }) => {
       <Button as="a" href="/" variant="inverse-primary">
         {formatMessage(messages.course)}
       </Button>
-      <Button as="a" href={urls.programsUrl()} variant="inverse-primary">
+      {/* <Button as="a" href={urls.programsUrl()} variant="inverse-primary">
         {formatMessage(messages.program)}
-      </Button>
+      </Button> */}
       <Button
         as="a"
         href={urls.baseAppUrl(courseSearchUrl)}
@@ -43,9 +43,9 @@ export const CollapseMenuBody = ({ isOpen }) => {
       >
         {formatMessage(messages.discoverNew)}
       </Button>
-      <Button as="a" href={getConfig().SUPPORT_URL} variant="inverse-primary">
+      {/* <Button as="a" href={getConfig().SUPPORT_URL} variant="inverse-primary">
         {formatMessage(messages.help)}
-      </Button>
+      </Button> */}
       {authenticatedUser && (
         <>
           {!!dashboard && (
